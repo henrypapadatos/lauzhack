55cf06443592d19eea410578591d848ca5978a6f
Henry papadatos <papadatoshenry@gmail.com>
2022-12-04 02:30:48 +0100

    Download mp3 file out of a Youtube URL

---
+++ youtube.py
@@ -0,0 +1,14 @@
+import os
+from pytube import YouTube
+
+def download_from_youtube(url = "https://www.youtube.com/watch?v=f_NoW_npKMA&ab_channel=HenryPapadatos"):
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    destination = '.'
+    out_file = video.download(output_path=destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    print(yt.title + " has been successfully downloaded.")
+
+    return new_file
